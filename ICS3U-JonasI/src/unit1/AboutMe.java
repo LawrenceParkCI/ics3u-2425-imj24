c4fc195b04df25b96cb3acdcb78bf8a910590f0b
jonas <jonas@10.123.162.69>
2024-09-24 11:30:56 -0400

    Finished all the information about me

---
+++ ICS3U-JonasI/src/unit1/AboutMe.java
@@ -0,0 +1,17 @@
+package unit1;
+
+public class AboutMe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hi, my name is Jonas Im");
+		System.out.println("I am a 15 year old boy in grade 11");
+		System.out.println("I was born on October 28, 2008");
+		System.out.println("My teacher is awesome and her name is Ms. Kemp");
+		System.out.println("I go to Lawrence Park Collegiate Institute in Toronto, Canada");
+		System.out.println("\"GO LPCI PANTHERS!");
+		System.out.println("Jonas Im");
+	}
+
+}
