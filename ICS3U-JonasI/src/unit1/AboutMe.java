d8be32a364ee917d141369aef7b729c7b6194a68
jonas <jonas@10.123.162.69>
2024-09-26 22:00:51 -0400

    time table finished

--- ICS3U-JonasI/src/unit1/AboutMe.java
+++ ICS3U-JonasI/src/unit1/AboutMe.java
@@ -1,5 +1,5 @@
 package unit1;
-
+ 
 public class AboutMe {
 
 	public static void main(String[] args) {
@@ -12,6 +12,34 @@ public class AboutMe {
 		System.out.println("I go to Lawrence Park Collegiate Institute in Toronto, Canada");
 		System.out.println("\"GO LPCI PANTHERS!");
 		System.out.println("Jonas Im");
-	}
+		
+		System.out.println("");
+		System.out.println("Timetable ");
+		
+		System.out.println("Week of september 23rd");
+		System.out.println("+------------------+-----------------------------------------+----------------------------------------+-----------------------------------------+-----------------------------------------+");
+        System.out.println("|       Day        |              Period 1                  |              Period 2                  |              Period 3                  |              Period 4                  |");
+        System.out.println("+------------------+-----------------------------------------+----------------------------------------+-----------------------------------------+-----------------------------------------+");
 
+        
+        System.out.println("|  Monday (Day 1)  |  9:00 AM - 10:15 AM (Computer Science) | 10:20 AM - 11:40 AM (Cooking)          | 12:40 PM -  1:55 PM (English)          |  2:00 PM -  3:15 PM (Functions)        |");
+        
+        System.out.println("+------------------+-----------------------------------------+----------------------------------------+-----------------------------------------+-----------------------------------------+");        
+        
+        System.out.println("|  Tuesday (Day 2) |  9:00 AM - 10:15 AM (Cooking)          | 10:20 AM - 11:40 AM (Computer Science) | 12:40 PM -  1:55 PM (Functions)        |  2:00 PM -  3:15 PM (English)          |");
+       
+        System.out.println("+------------------+-----------------------------------------+----------------------------------------+-----------------------------------------+-----------------------------------------+");
+        
+        System.out.println("| Wednesday (Late) |  9:55 AM - 10:55 AM (Computer Science) | 11:00 AM - 12:05 PM (Cooking)          |  1:05 PM -  2:10 PM (English)          |  2:15 PM -  3:15 PM (Functions)        |");
+
+        System.out.println("+------------------+-----------------------------------------+----------------------------------------+-----------------------------------------+-----------------------------------------+");
+        
+        System.out.println("| Thursday (Day 2) |  9:00 AM - 10:15 AM (Cooking)          | 10:20 AM - 11:40 AM (Computer Science) | 12:40 PM -  1:55 PM (Functions)        |  2:00 PM -  3:15 PM (English)          |");
+
+        System.out.println("+------------------+-----------------------------------------+----------------------------------------+-----------------------------------------+-----------------------------------------+");
+    
+        System.out.println("|  Friday (Day 1)  |  9:00 AM - 10:15 AM (Computer Science) | 10:20 AM - 11:40 AM (Cooking)          | 12:40 PM -  1:55 PM (English)          |  2:00 PM -  3:15 PM (Functions)        |");
+
+        System.out.println("+------------------+-----------------------------------------+----------------------------------------+-----------------------------------------+-----------------------------------------+");
+    }
 }
