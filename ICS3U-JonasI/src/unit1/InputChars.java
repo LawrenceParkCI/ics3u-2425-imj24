4b128cef00d4d31da5dbeb98c0b90d3d24855e0b
jonas <jonas@10.123.162.69>
2024-10-03 09:54:32 -0400

    Forgot to add header but I added it now

---
+++ ICS3U-JonasI/src/unit1/InputChars.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package unit1;
+
+/**
+ * 
+ */
+public class InputChars {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
