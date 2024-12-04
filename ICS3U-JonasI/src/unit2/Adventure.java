2de7cd5379d0f68f6e42f005543253547b20dcee
jonas <jonas@localhost>
2024-12-04 10:58:25 -0500

    Almost done

---
+++ ICS3U-JonasI/src/unit2/Adventure.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package unit2;
+
+/**
+ * 
+ */
+public class Adventure {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
